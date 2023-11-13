ed27b9d91b747267ef444a2e8785ca3afba18e11
Jose Gonzalo Scali <gonzalojoses@hotmail.com>
2023-11-13 16:37:38 -0300

    mejoras

--- BackEnd/ApiComm/main.py
+++ BackEnd/ApiComm/main.py
@@ -1,16 +1,11 @@
 from fastapi import FastAPI, HTTPException
 from emails.classes.c_mail import EmailSchema
 from emails.funcs.send_mail import enviar_correo
-from discord_notif.bot.discord_bot import bot, send_message_to_general_channel
-from discord_notif.discord_schemas.msj_schema import Message
 import threading
 import os
-TOKEN = os.getenv('DISCORD_SECRET_TOKEN')
+
 app = FastAPI()
-general_channel_id = 'general'
-# Iniciar el bot de Discord en un hilo aparte
-bot_thread = threading.Thread(target=bot.run(TOKEN), args=())
-bot_thread.start()
+
 
 
 
